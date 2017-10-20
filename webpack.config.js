e15dfc6347956f4c3bfea651f82137427330487e
Thomas <thomasshannon1117@gmail.com>
2017-10-20 14:02:15 -0700

    FE BUILD:
    - added bootstrap to project
    - added file loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -13,15 +13,25 @@ module.exports = {
     },
     module: {
         rules: [
+        { 
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+            use: [{
+                loader: 'url-loader?limit=100000'
+            }]
+        },
         {
             test: /\.scss$/,
-            use: [{
+            use: [
+            {
                 loader: "style-loader" // creates style nodes from JS strings
-            }, {
+            }, 
+            {
                 loader: "css-loader" // translates CSS into CommonJS
-            }, {
+            }, 
+            {
                 loader: "sass-loader" // compiles Sass to CSS
-            }]
+            },
+            ]
         },
         {
             test: /\.js$/,
