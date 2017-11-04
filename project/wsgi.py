2c4263c75cfd17fd1a0226ca8e8cf616605d58b5
Thomas <thomasshannon1117@gmail.com>
2017-11-04 15:16:20 -0700

    test

--- project/wsgi.py
+++ project/wsgi.py
@@ -7,11 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from __future__ import absolute_import, unicode_literals
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.production")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
