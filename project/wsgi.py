f514da50dc3f67747569ebe34e993bf80eeb9b14
Thomas <thomasshannon1117@gmail.com>
2017-11-04 15:14:02 -0700

    - updated wsgi

--- project/wsgi.py
+++ project/wsgi.py
@@ -7,12 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-from __future__ import absolute_import, unicode_literals
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
